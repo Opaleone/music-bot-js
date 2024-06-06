0fdb401a405a2c4d319748c9c4d7f51e0d029c57
Leone Varando <lvarando@gmail.com>
2024-06-06 11:23:41 -0500

    Revert "Added boilerplate code for bot creation"
    This reverts commit afa627428cbb5c7c540ef651842b5c727332d51a.

--- utils/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  sleep: async (t) => {
-    // sleeps for a random amount of time between 0 and t
-    const randomMs = Math.floor(Math.random() * t)
-    await new Promise(res => setTimeout(res, randomMs));
-  }
-}
