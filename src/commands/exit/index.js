ef32b39d0a6ed79ef911332554dbc5bf876ed665
Leone Varando <lvarando@gmail.com>
2024-06-07 23:55:22 -0500

    Added more commands

---
+++ src/commands/exit/index.js
@@ -0,0 +1,29 @@
+const { SlashCommandBuilder } = require('discord.js');
+const fs = require("fs");
+const config = require('../../../config.json')
+
+module.exports = {
+  data: new SlashCommandBuilder()
+      .setName('exit')
+      .setDescription('Exits current voice channel'),
+  async execute(interaction) {
+    try {
+      // for debugging
+      if (config.debug.status) {
+        if (!config.debug.channels.includes(interaction.channelId)) {
+          return await interaction.reply({ content: "Currently testing bot. Try again later!", ephemeral: true });
+        }
+      }
+
+      
+    } catch (e) {
+      const todayDate = new Date().toJSON();
+      const msg = `${todayDate}: ${e} ::::\n`;
+
+      // Log error to file
+      fs.appendFile('errors.log', msg, (err) => {
+        if (err) console.error(err)
+      })
+    }
+  }
+}
